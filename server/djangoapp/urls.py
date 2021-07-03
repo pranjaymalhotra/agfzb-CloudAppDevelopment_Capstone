68185d532f460d80f2dcd867f9287dd687919825
PranjayMalhotra <Pranjay.100@gmail.com>
2021-07-03 11:51:03 +0000

    Adding temporary changes to Github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,24 +5,18 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
+    #path(route='static_page', view=views.simple_django_view, name='static_page'),
 
-    # path for about view
-
-    # path for contact us view
-
+    path(route='about', view=views.get_about, name='about'),
+    path(route='contact', view=views.get_contact, name='contact'),
     # path for registration
-
-    # path for login
-
-    # path for logout
+    
+    path(route='login', view=views.get_login, name='login'),
+    path(route='logout', view=views.get_logout, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
-
+    path(route='registration', view=views.registration_request, name='registration'),
+    path(route='signup_view', view=views.signup_view, name='signup_view'),
+    #path ( 'dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details' ),
+    #path ( 'r_eview/<int:rev_id>', views.add_review, name="add_review" ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -18,24 +18,66 @@ logger = logging.getLogger(__name__)
 
 
 # Create an `about` view to render a static about page
-# def about(request):
-# ...
+def get_about(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/about.html', context)
 
 
 # Create a `contact` view to return a static contact page
-#def contact(request):
+def get_contact(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/contact.html', context)
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def get_login(request):
+    if request.method=='POST':
+        user = request.POST[ 'username' ]
+        password = request.POST[ 'psw' ]
+        user = authenticate( username=user, password=password )
+        if user is not None:
+            login( request, user )
+        return redirect( 'djangoapp:index' )
+    return HttpResponseRedirect( reversed( 'index' ) )
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def get_logout(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/logout.html', context)
 
-# Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+def signup_view( request ):
+    context = {
+        'err': ''
+    }
+    return render( request, 'djangoapp/registration.html', context ) 
+def registration_request(request):
+    if request.method == 'POST':
+        user_exist = False
+        username = request.POST[ 'username' ]
+        email = request.POST[ 'email' ]
+        f_name = request.POST[ 'f_name' ]
+        l_name = request.POST[ 'l_name' ]
+        psw = request.POST[ 'psw' ]
+        psw2 = request.POST[ 'psw2' ]
+        if psw != psw2:
+            return render( request, 'djangoapp/registration.html', {
+                'err': 'Error! Passwords do not match... {0}, {1}'.format( psw, psw2 )
+            } )  
+        try:
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            logger.debug("{} New user".format(username))
+        if not user_exist:
+            user = User.objects.create_user( username, email, psw, first_name=f_name, last_name=l_name )
+            return redirect("djangoapp:index")
+        else:
+            return render(request, 'djangoapp/registration.html', 
+            {
+                'err': 'You are already Registered'
+            })
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
