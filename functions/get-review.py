b792b0fe1fa3b62c605dc6e8246c380a0a4494d4
Pranjay Malhotra <69764093+pranjaymalhotra@users.noreply.github.com>
2021-07-05 15:43:31 +0530

    Update get-review.py

--- functions/get-review.py
+++ functions/get-review.py
@@ -1,13 +1,28 @@
-#
-#
-# main() will be run when you invoke this action
-#
-# @param Cloud Functions actions accept a single parameter, which must be a JSON object.
-#
-# @return The output of this action, which must be a JSON object.
-#
-#
 import sys
+import requests
 
-def main(dict):
-    return { 'message': 'Hello world' }
+
+
+def main(params):
+    URL = params['cloudant_url'] + '/reviews/_design/api/_view/by-dealership?include_docs=True'
+    if 'dealerId' in params.keys():
+        URL = URL + '&key=' + str(int(params['dealerId']))
+    req = requests.get(URL, auth=(params['cloudant_user'], params['cloudant_password']))
+    if req.status_code != 200:
+        return {
+            'headers': {'Content-Type': 'application/json'},
+            'statusCode': 500,
+            'body': {'error' :req.text}
+        }
+    payload = req.json()
+    if len(payload['rows']) <= 0:
+        return {
+            'headers': {'Content-Type': 'application/json'},
+            'statusCode': 404,
+            'body': []
+        }
+    return {
+            'headers': {'Content-Type': 'application/json'},
+            'statusCode': 200,
+            'body': list(map(lambda d: d['doc'], payload['rows']))
+        }
